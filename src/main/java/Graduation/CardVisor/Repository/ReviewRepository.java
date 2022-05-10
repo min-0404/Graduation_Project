09f7a495a4879452da97cc1d1fed9a394288ecfa
yung5060 <80459033+yung5060@users.noreply.github.com>
2022-05-10 13:01:54 +0900

    Delete src/main/java/Graduation/CardVisor/Repository directory
    2 repository packages (Capital case Repository deleted)

--- src/main/java/Graduation/CardVisor/Repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package Graduation.CardVisor.repository;
-
-import Graduation.CardVisor.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
