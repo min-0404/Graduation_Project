41e7fc9b1d8999eba2d4eeb99831823f62c98ff0
yung5060 <yung126262@naver.com>
2022-05-12 23:38:51 +0900

    .

--- src/main/java/Graduation/CardVisor/repository/ServiceOneRepository.java
+++
@@ -1,7 +0,0 @@
-package Graduation.CardVisor.repository;
-
-import Graduation.CardVisor.domain.ServiceOne;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceOneRepository extends JpaRepository<ServiceOne, Long> {
-}
