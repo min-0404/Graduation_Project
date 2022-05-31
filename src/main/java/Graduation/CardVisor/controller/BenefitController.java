5ea75a6c2841669c30ef05f4748d26700f636b1f
yung5060 <yung126262@naver.com>
2022-05-31 23:28:01 +0900

    2022/5/31

--- src/main/java/Graduation/CardVisor/controller/BenefitController.java
+++ src/main/java/Graduation/CardVisor/controller/BenefitController.java
@@ -5,14 +5,20 @@ import Graduation.CardVisor.domain.serviceone.ServiceOneDto;
 import Graduation.CardVisor.service.BenefitService;
 import Graduation.CardVisor.service.CardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +28,27 @@ public class BenefitController {
     private final BenefitService benefitService;
 
 
+    public List<Long> serviceOneCards() {
+        var uri = UriComponentsBuilder.fromUriString("http://localhost:5001")
+                .build()
+                .encode()
+                .toUri();
+
+        var headers = new HttpHeaders();
+        var httpEntity = new HttpEntity<>(headers);
+
+        var responseType = new ParameterizedTypeReference<List<Long>>(){};
+        var responseEntity = new RestTemplate().exchange(
+                uri,
+                HttpMethod.GET,
+                httpEntity,
+                responseType
+        );
+        return responseEntity.getBody();
+    }
+
+    public List<Long> results = new ArrayList<>();
+
     // 추천 서비스 1 : 선택된 혜택들을 받아주는 컨트롤러
     @CrossOrigin("http://localhost:3000") // method: post 로 받을 시 꼭 붙여줄 것
     @PostMapping("/select")
@@ -30,6 +57,18 @@ public class BenefitController {
         for(ServiceOneDto serviceOneDto : list){
             benefitService.DtoToServiceOne(serviceOneDto); // Dto 를 정식 ServiceOne 객체로 변환해줌
         }
+
+        results = serviceOneCards();
+    }
+
+    @GetMapping("/results")
+    public List<Long> results() {
+
+        for(Long result : results) {
+            System.out.println(result);
+        }
+
+        return results;
     }
 
 }
