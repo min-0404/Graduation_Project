8c1faa44fee38e6b9cc2cad2c40d9b9a173dac14
Yung Cho <yung5060@kakao.com>
2022-10-19 21:49:06 +0900

    .

---
+++ src/main/java/Graduation/CardVisor/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package Graduation.CardVisor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+// 3000 포트, 5000 포트와 CorsPolicy 허용하기 위한 config 설정
+// @CrossOrigin 어노테이션 사용하는 것보다 나음
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // 3000번 포트 접근 허용
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
