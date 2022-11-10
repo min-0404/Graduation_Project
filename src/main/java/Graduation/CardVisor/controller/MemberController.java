aa9f1a1e92e856708f358aee76142ced7338fba2
Yung Cho <yung5060@kakao.com>
2022-11-10 14:55:01 +0900

    adding showMyInfo API function (20221110)

--- src/main/java/Graduation/CardVisor/controller/MemberController.java
+++ src/main/java/Graduation/CardVisor/controller/MemberController.java
@@ -1,13 +1,20 @@
 package Graduation.CardVisor.controller;
 
 
+import Graduation.CardVisor.domain.member.Member;
 import Graduation.CardVisor.domain.member.MyInfoDto;
+import Graduation.CardVisor.repository.MemberRepository;
+import Graduation.CardVisor.service.AdminService;
 import Graduation.CardVisor.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,12 +23,26 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
+
+    private final AdminService adminService;
+
+    private final MemberRepository memberRepository;
     @GetMapping("/username")
     public String memberUsername() {
 
         Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
 
         return loggedInUser.getName();
+    }
+
+    @GetMapping("/showMyInfo")
+    public ResponseEntity<?> showAgeGender() {
+        Long memberId = adminService.authenticate();
+        Member member = memberRepository.findMemberById(memberId);
+        Map<String, String> store = new HashMap<>();
+        store.put("gender", member.getGender().toString());
+        store.put("age", member.getAge().toString());
+        return ResponseEntity.ok().body(store);
     }
 
     @PostMapping("/myInfo")
