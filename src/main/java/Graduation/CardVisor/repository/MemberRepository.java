86fbd1da3fdd362106de48f8f315b864514f8529
Yung Cho <yung5060@kakao.com>
2022-11-22 19:59:18 +0900

    improvements on the donut api (20221122)

--- src/main/java/Graduation/CardVisor/repository/MemberRepository.java
+++ src/main/java/Graduation/CardVisor/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package Graduation.CardVisor.repository;
 import Graduation.CardVisor.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -13,4 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     public Member getMemberById(Long memberId);
 
     Member findMemberById(Long id);
+
+    public List<Member> findByAgeAndGender(Member.Age age, Member.Gender gender);
 }
--- src/main/java/Graduation/CardVisor/service/DonutsService.java
+++ src/main/java/Graduation/CardVisor/service/DonutsService.java
@@ -1,5 +1,6 @@
 package Graduation.CardVisor.service;
 
+import Graduation.CardVisor.domain.member.Member;
 import Graduation.CardVisor.domain.servicetwo.ServiceTwo;
 import Graduation.CardVisor.repository.MemberRepository;
 import Graduation.CardVisor.repository.ServiceTwoRepository;
@@ -25,91 +26,28 @@ public class DonutsService {
 
         // 현재 로그인된 계정 확인 및 나이, 성별 조회
         Long memberId = adminService.authenticate();
-        String age = String.valueOf(memberRepository.getMemberById(memberId).getAge());
-        String gender = String.valueOf(memberRepository.getMemberById(memberId).getGender());
+        Member.Age age = memberRepository.getMemberById(memberId).getAge();
+        Member.Gender gender = memberRepository.getMemberById(memberId).getGender();
 
-        // 로그인된 계정과 동일한 나이/성별의 cost 데이터를 result 리스트에 담아 반환
-        List<Float> result = new ArrayList<>();
-        if (age.equals("one")) {
-            if (gender.equals("male")) {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901100L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            } else {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901101L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
+        List<Member> memberList = memberRepository.findByAgeAndGender(age, gender);
+
+        int cnt = 0;
+
+        // 로그인된 계정과 동일한 나이/성별의 cost 데이터를 평균내고 result 리스트에 담아 반환
+        List<Float> result = new ArrayList<>(List.of(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f));
+        for (Member member : memberList) {
+            List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(member.getId());
+            if (costs.isEmpty()) {
+                continue;
             }
-        }
-        if (age.equals("two")) {
-            if (gender.equals("male")) {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901200L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            } else {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901201L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
+            for (int i = 0; i < 15; i++) {
+                result.set(i, result.get(i) + costs.get(i).getCost());
             }
+            cnt++;
         }
-        if (age.equals("three")) {
-            if (gender.equals("male")) {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201930010L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            } else {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201930011L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            }
+        for (int i = 0; i < 15; i++) {
+            result.set(i, result.get(i) / cnt);
         }
-        if (age.equals("four")) {
-            if (gender.equals("male")) {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901400L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            } else {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901401L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            }
-        }
-        if (age.equals("five")) {
-            if (gender.equals("male")) {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901500L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            } else {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901501L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            }
-        }
-        if (age.equals("six")) {
-            if (gender.equals("male")) {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901600L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            } else {
-                List<ServiceTwo> costs = serviceTwoRepository.findByMemberId(201901601L);
-                for (ServiceTwo s : costs) {
-                    result.add(s.getCost());
-                }
-            }
-        }
-        log.info(age);
-        log.info(gender);
         return result;
     }
 }
